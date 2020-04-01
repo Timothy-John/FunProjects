047a11a26b768dbc25e4eed3ca4a151c2236722f
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2020-04-02 00:16:59 +0530

    Fun Project with Python

--- Python/BibleBooks/solution.py
+++ Python/BibleBooks/solution.py
@@ -1,20 +1,36 @@
 import re
 
-passage=open('BibleBooksQuestion.txt','r')
-passage=passage.read()
-passage=passage.lower()
-passage=re.sub('[0-9]','',passage)
-passage=re.sub('[.,()]','',passage)
-passage=re.sub(' ','',passage)
 
-books=open('books.txt','r')
-books=books.read()
-books=books.lower()
-books=re.sub('[0-9]','',books)    # can use \d instead of 0-9
-books=re.sub(' ','',books)
-books=re.sub('\n',"|",books)      # | means OR
+def solution(question):
+    if question==1:
+        passage=open('BibleBooksQuestion1.txt','r')
+    elif question==2:
+        passage=open('BibleBooksQuestion2.txt','r')
+    else:
+        print("Enter 1 or 2 ONLY.....")
+        repeat()
 
-print(passage)
-r=re.compile(books)
-print("\n",books)
-print("\n",r.findall(passage),"\n")
+    passage=passage.read()
+    passage=passage.lower()
+    passage=re.sub('[0-9]','',passage)
+    passage=re.sub('[.,()]','',passage)
+    passage=re.sub(' ','',passage)
+
+    books=open('books.txt','r')
+    books=books.read()
+    books=books.lower()
+    books=re.sub('[0-9]','',books)    # can use \d instead of 0-9
+    books=re.sub(' ','',books)
+    books=re.sub('\n',"|",books)      # | means OR
+
+    print(passage)
+    r=re.compile(books)
+    print("\n",books)
+    print("\n",r.findall(passage),"\n")
+
+def repeat():
+    c=int(input("Question 1 or Question 2 :  "))
+    solution(c)    
+
+c=int(input("Question 1 or Question 2 :  "))
+solution(c)
